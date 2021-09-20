a6e5bae814ac864dd2f5578cd45aeea92ffd5d54
Safa Nasir <49034994+safakhan413@users.noreply.github.com>
2021-09-20 13:04:45 -0400

    a

--- flights-delay-pred.py
+++ flights-delay-pred.py
@@ -1,2 +1,15 @@
-python -m ipykernel install --user --name=fligh-delay-pred-feDiYCl6
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+flights=pd.read_csv("flights.csv")
+print(flights.head(10))
+flights.shape
+sns.countplot(x="CANCELLATION_REASON",data=flights)
+plt.show()
+#Reason for Cancellation of flight: A - Airline/Carrier; B - Weather; C - National Air System; D - Security
+sns.countplot(x="MONTH",hue="CANCELLATION_REASON",data=flights)
+plt.show()
+print(flights["CANCELLATION_REASON"].unique())
+print
